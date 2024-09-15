aaaa3e33e830fdf61b0bdeca02fa217db52671e4
Yohannes-Ayalew <123648932+Yohannes-Ayalew@users.noreply.github.com>
2024-09-15 15:45:03 +0300

    Update header_link.php

--- header_link.php
+++ header_link.php
@@ -11,20 +11,20 @@
 
     <!-- All css files are included here. -->
     <!-- Bootstrap fremwork main css -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="Css/bootstrap.min.css">
     <!-- Owl Carousel  main css -->
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="Css/owl.carousel.min.css">
+    <link rel="stylesheet" href="Css/owl.theme.default.min.css">
     <!-- This core.css file contents all plugings css file. -->
-    <link rel="stylesheet" href="css/core.css">
+    <link rel="stylesheet" href="Css/core.css">
     <!-- Theme shortcodes/elements style -->
-    <link rel="stylesheet" href="css/shortcode/shortcodes.css">
+    <link rel="stylesheet" href="Css/shortcode/shortcodes.css">
     <!-- Theme main style -->
     <link rel="stylesheet" href="style.css">
     <!-- Responsive css -->
-    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="Css/responsive.css">
     <!-- User style -->
-    <link rel="stylesheet" href="css/custom.css">
+    <link rel="stylesheet" href="Css/custom.css">
 
 
     <!-- Modernizr JS -->
